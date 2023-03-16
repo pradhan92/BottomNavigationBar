591ea225754ac49fec407c6d329cd8cf64e6af91
pradhan92 <pradhantk92@gmail.com>
2023-03-16 11:42:20 +0900

    first commit

---
+++ lib/pages/chart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ChartPage extends StatelessWidget {
+  const ChartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("chartPage")),
+    );
+  }
+}
---
+++ lib/view/my_home_page.dart
@@ -0,0 +1,71 @@
+import 'package:demo2/pages/chart.dart';
+import 'package:demo2/pages/home.dart';
+import 'package:demo2/pages/menu.dart';
+import 'package:demo2/pages/profile.dart';
+import 'package:demo2/pages/setting.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
+        showSelectedLabels: true,
+        selectedItemColor: Colors.blue,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.white,
+        onTap: (index) {
+          //print(index);
+          selectedIndex = index;
+          setState(() {});
+        },
+        currentIndex: selectedIndex,
+        items:const [
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.green
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.graphic_eq_outlined),
+            backgroundColor: Colors.green,
+            label: 'Chart',
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Menu',
+            backgroundColor: Colors.green
+
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt_outlined),
+            label: 'Profile',
+            backgroundColor: Colors.green
+
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+            backgroundColor: Colors.green
+
+          ),
+        ],
+      ),
+      body: 
+      selectedIndex == 0 ? const HomePage()
+      :selectedIndex == 1 ? const ChartPage()
+      :selectedIndex == 2 ? const MenuPage()
+      :selectedIndex == 3 ? const ProfilePage()
+      :const SettingPage(),
+    );
+  }
+}
